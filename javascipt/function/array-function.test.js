ad7a549eaac25d8212153ba112f778b6e382006e
HaoWang-TW <hawang@thoughtworks.com>
2018-04-05 22:54:29 +0800

    add practice for array function

---
+++ javascipt/function/array-function.test.js
@@ -0,0 +1,14 @@
+describe('array function', () => {
+  let obj = {
+    a: 1,
+    getA() {
+      return this.a
+    },
+    getAByArrayFunction: () => this.a
+  };
+
+  test('array function will get undefined', () => {
+    expect(obj.getA()).toBe(1);
+    expect(obj.getAByArrayFunction()).toBeUndefined();
+  });
+});
